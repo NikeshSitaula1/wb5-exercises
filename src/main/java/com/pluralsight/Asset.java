7b4b2ac1e2f16f9e8f3e11ae0624ec6deb46422d
Nikesh Sitaula <sitaulanike@gmail.com>
2024-10-29 10:56:05 -0400

    Inheritance example

---
+++ src/main/java/com/pluralsight/Asset.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Asset {
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    private double value;
+}
